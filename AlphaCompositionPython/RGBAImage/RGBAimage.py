be9e19890e84f8dea597889300055222a1c3641b
Edoardo Bonanni <edobona98@gmail.com>
2022-12-13 16:20:26 +0100

    Add files via upload

--- AlphaCompositionPython/RGBAImage/RGBAimage.py
+++ AlphaCompositionPython/RGBAImage/RGBAimage.py
@@ -8,7 +8,7 @@ class RGBAImage:
 
         self._height = self._image.shape[0]
         self._width = self._image.shape[1]
-        self._num_channels = self._image.shape[2]
+        self._numChannels = self._image.shape[2]
 
     def getWidth(self):
         return self._width
@@ -17,22 +17,22 @@ class RGBAImage:
         return self._height
 
     def getNumChannels(self):
-        return self._num_channels
+        return self._numChannels
 
     def getSize(self):
-        return self.getWidth() * self.getHeight() * self.getNumChannels()
+        return self._width * self._height * self._numChannels
 
     def createFlatImage(self):
         size = self.getSize()
         flatImage = np.zeros(shape=size)
         # scroll by rows
-        for i in range(0, self.getHeight()):
-            for j in range(0, self.getWidth()):
+        for i in range(0, self._height):
+            for j in range(0, self._width):
                 pixel = self._image[i][j]
-                flatImage[(i * self.getWidth() * self.getNumChannels()) + (j * self.getNumChannels())] = float(pixel[2])
-                flatImage[(i * self.getWidth() * self.getNumChannels()) + (j * self.getNumChannels()) + 1] = float(pixel[1])
-                flatImage[(i * self.getWidth() * self.getNumChannels()) + (j * self.getNumChannels()) + 2] = float(pixel[0])
-                flatImage[(i * self.getWidth() * self.getNumChannels()) + (j * self.getNumChannels()) + 3] = float(pixel[3])
+                flatImage[(i * self._width * self._numChannels) + (j * self._numChannels)] = float(pixel[2])
+                flatImage[(i * self._width * self._numChannels) + (j * self._numChannels) + 1] = float(pixel[1])
+                flatImage[(i * self._width * self._numChannels) + (j * self._numChannels) + 2] = float(pixel[0])
+                flatImage[(i * self._width * self._numChannels) + (j * self._numChannels) + 3] = float(pixel[3])
         return flatImage
 
     def showImage(self):
